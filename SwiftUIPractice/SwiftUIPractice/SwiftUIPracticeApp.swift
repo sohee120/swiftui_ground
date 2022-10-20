ec69337b6bffe283050f1fdcdfc43ce1beb94327
sohee120 <yuhsohee120@gmail.com>
2022-10-20 09:35:10 +0900

    Add: SwiftUI Slider and Picker

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by 윤소희 on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
