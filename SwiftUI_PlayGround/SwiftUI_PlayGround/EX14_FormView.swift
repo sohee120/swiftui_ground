a9f0040bbce44a5ab1e49d1c84782fb79a62e3ea
sohee120 <yuhsohee120@gmail.com>
2022-11-21 13:39:30 +0900

    Add:SwiftUI FormView

--- SwiftUI_PlayGround/SwiftUI_PlayGround/EX14_FormView.swift
+++ SwiftUI_PlayGround/SwiftUI_PlayGround/EX14_FormView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct EX14_FormView: View {
+    
+    @State private var username: String = ""
+    @State private var notificationEnabled: Bool = false
+    @State private var isPrivate: Bool = false
+    @State private var previewIndex: Int = 0
+    let previewOptions: Array<String> = ["Always","When Unlocked","Never"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            Form{
+                Section(header: Text("PROFILE")) {
+                    TextField("Username", text: $username)
+                    Toggle(isOn: $isPrivate) {
+                        Text("isPrivate")
+                    }
+                } //S1
+                
+                Section(header: Text("NOTIFICATIONS")) {
+                    Toggle(isOn: $notificationEnabled) {
+                        Text("Enabled")
+                    }
+                    Picker(selection: $previewIndex, label: Text("Show Previews")) {
+                        ForEach(0..<previewOptions.count) {idx in
+                            Text("\(previewOptions[idx])")
+                        }
+                    }
+                } //S2
+                
+                Section(header: Text("ABOUT")) {
+                    HStack{
+                        Text("Vesion")
+                        Spacer()
+                        Text("2.0.1")
+                    }
+                } //S3
+                
+                Section{
+                    Button {
+                        print("Clicked Button")
+                    } label: {
+                        Text("Reset All Settings")
+                    }
+                } //S4
+            }//FR
+            .navigationBarTitle(Text("Settings"))
+        }//NV
     }
 }
 
